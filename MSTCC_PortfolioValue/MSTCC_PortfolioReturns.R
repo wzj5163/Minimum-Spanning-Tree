dfeaeea0348939c53d029986ca11c5c3479f0963
wzj5163 <81182810+wzj5163@users.noreply.github.com>
2022-04-01 09:05:06 +0100

    Add files via upload

---
+++ MSTCC_PortfolioValue/MSTCC_PortfolioReturns.R
@@ -0,0 +1,15 @@
+rm(list = ls())
+
+setwd("~/Documents/METIS/Minimum Spanning Tree/Codes/MSTCC_PortfolioValue")
+
+#Plot returns
+prezzi<-read.table("rend100_crypto.csv", header=TRUE, sep=",", dec=".")
+ZOO <- zoo(prezzi[,-1], order.by=as.Date(as.character(prezzi$Date), format='%m/%d/%Y'))
+#dev.new()
+png("MSTCC_PortRet1.png", width=500, height=400, bg = "transparent")
+plot(ZOO, screens=1, col=c("black", "lightblue", "pink","darkslateblue","red","brown", "gold", "grey", "green", "blue3","darkorchid2"), ylab="", xlab="")
+dev.off()
+#dev.new()
+png("MSTCC_PortRet2.png", width=500, height=400, bg = "transparent")
+plot(ZOO[,c(1,2,3,4,11)], screens=1, col=c("black", "lightblue", "pink","darkslateblue", "darkorchid2"), lwd=1, ylab="", xlab="")
+dev.off()
